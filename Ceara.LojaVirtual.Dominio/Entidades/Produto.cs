937168bdb525de489053fbaaa0f9c0e39ed9f01c
wesleysrodrigues <wesleyp.bsi@gmail.com>
2016-03-02 16:47:15 -0300

    Recriar projeto dominio

--- Ceara.LojaVirtual.Dominio/Entidades/Produto.cs
+++ Ceara.LojaVirtual.Dominio/Entidades/Produto.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Ceara.LojaVirtual.Dominio.Entidade
+﻿namespace Quiron.LojaVirtual.Dominio.Entidades
 {
     public class Produto
     {
